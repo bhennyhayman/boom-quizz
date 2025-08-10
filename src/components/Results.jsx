765a37b72c0f71de267551d3c3c7bc3c0cd0cf36
Bhenny Hayman <bengero686@gmail.com>
2025-08-10 14:29:03 +0000

    vercel.json file added

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import questions from '../data/questions.js';
 import { Link, useLocation } from 'react-router-dom';
 
 export default function Results() {
     const location = useLocation();
-    const state = location.state || {};
-    const { score, selectedOptions } = state;
+    const [score, setScore] = useState(null);
+    const [selectedOptions, setSelectedOptions] = useState([]);
+
+    useEffect(() => {
+        const state = location.state || {};
+        const storedScore = localStorage.getItem('score');
+        const storedSelectedOptions = localStorage.getItem('selectedOptions');
+
+        if (state.score !== undefined && state.selectedOptions !== undefined) {
+            setScore(state.score);
+            setSelectedOptions(state.selectedOptions);
+        } else if (storedScore !== null && storedSelectedOptions !== null) {
+            setScore(parseInt(storedScore, 10));
+            setSelectedOptions(JSON.parse(storedSelectedOptions));
+        }
+    }, [location.state]);
 
     return (
         <div>
@@ -14,14 +28,14 @@ export default function Results() {
             <h1 className='underline m-2'>Results</h1>
             
             {score !== null && <>
-              <div className="score">Your score: {score || 0}/{questions.length}</div>
+              <div className="score">Your score: {score}/{questions.length}</div>
             </>}
 
             {questions.map((item, index) => 
-            <div key={index} className={`m-4 p-4 border-2 border-gray-300 rounded-lg ${selectedOptions && selectedOptions[index] !== item.answer ? 'bg-red-200' : 'bg-green-200'}`}>
+            <div key={index} className={`m-4 p-4 border-2 border-gray-300 rounded-lg ${selectedOptions[index] !== item.answer ? 'bg-red-200' : 'bg-green-200'}`}>
               <div>{index+1}. {item.question}</div>
               <div className='p-2'>Answer: {item.answer}</div>
-              <div className='p-2'>Your answer: {selectedOptions ? selectedOptions[index] : 'N/A'}</div>
+              <div className='p-2'>Your answer: {selectedOptions[index] !== null && selectedOptions[index] !== undefined ? selectedOptions[index] : 'N/A'}</div>
             </div>
           )}
           
